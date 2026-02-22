77dfc61243cb1f1b50e52d59871d344bee68bb88
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-02-23 00:26:34 +0530

    [Riya]:added for quantity length

---
+++ src/main/java/com/app/quantitymeasurementapp/QuantityLength.java
@@ -0,0 +1,49 @@
+package com.app.quantitymeasurementapp;
+
+import java.util.Objects;
+
+public class QuantityLength {
+	// Attribute
+		private final double value;
+		private final LengthUnit unit;
+
+		// Constructor
+		public QuantityLength(double value, LengthUnit unit) {
+
+			if (unit == null) {
+				throw new IllegalArgumentException("Unit cannot be null");
+			}
+
+			this.value = value;
+			this.unit = unit;
+		}
+
+		// method convert to base unit
+		private double convertToBaseUnit() {
+			return this.value * unit.getConversionFactor();
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+
+			if (this == obj)
+				return true;
+
+			if (obj == null || getClass() != obj.getClass())
+				return false;
+
+			QuantityLength other = (QuantityLength) obj;
+
+			return Double.compare(this.convertToBaseUnit(), other.convertToBaseUnit()) == 0;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(convertToBaseUnit());
+		}
+
+		@Override
+		public String toString() {
+			return "Quantity(" + value + ", " + unit + ")";
+		}
+}

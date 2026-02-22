c07e50afacabafbf5ab5f9ff8d2b17188d283f32
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-02-23 00:27:42 +0530

    [Riya]:added for the updating QuantityMeasurementApp for UC3 added Quantity length instead of Inches and Feet

--- src/main/java/com/app/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -3,25 +3,22 @@ package com.app.quantitymeasurementapp;
 import java.util.Objects;
 
 public class QuantityMeasurementApp {
-	public static boolean checkFeetEquality(double value1, double value2) {
-        Feet f1 = new Feet(value1);
-        Feet f2 = new Feet(value2);
-        return f1.equals(f2);
-    }
+	public static void main(String[] args) {
 
-	 public static boolean checkInchesEquality(double value1, double value2) {
-	        Inches i1 = new Inches(value1);
-	        Inches i2 = new Inches(value2);
-	        return i1.equals(i2);
-	    }
-	 
-    public static void main(String[] args) {
-    		
-    		//check feet equality
-    	  System.out.println("Feet 1.0 & 1.0 Equal? "+ checkFeetEquality(11.0, 11.0));
-               
-    	  //check inches equality
-          System.out.println("Inches 1.0 & 1.0 Equal? " + checkInchesEquality(1.0, 1.0));
-                 
+        // Cross-unit comparison
+		QuantityLength l1 = new QuantityLength(1.0, LengthUnit.FEET);
+		QuantityLength l2 = new QuantityLength(12.0, LengthUnit.INCH);
+
+		// Results of comparison
+        System.out.println("Input values : " + l1 + " and " + l2);
+        System.out.println("Output: Equal " + l1.equals(l2) );
+
+        // Same-unit comparison
+        QuantityLength l3 = new QuantityLength(12.0, LengthUnit.INCH);
+        QuantityLength l4 = new QuantityLength(12.0, LengthUnit.INCH);
+
+        // Results of comparison
+        System.out.println("Input values: " + l3 + " and " + l4);
+        System.out.println("Output: Equal " + l3.equals(l4) );
     }
 }

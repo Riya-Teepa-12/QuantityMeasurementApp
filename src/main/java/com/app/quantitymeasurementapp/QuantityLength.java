6ca9e7f65e962655485d4ef409994c0e9dd8af8b
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-02-23 09:59:54 +0530

    [Riya]: added yard and centimeter in Quantitylength class

--- src/main/java/com/app/quantitymeasurementapp/QuantityLength.java
+++ src/main/java/com/app/quantitymeasurementapp/QuantityLength.java
@@ -18,37 +18,69 @@ public class QuantityLength {
 			this.unit = unit;
 		}
 
-		// method convert to base unit
+		// Method to convert the given length to base unit (Inches)
 		private double convertToBaseUnit() {
 			return this.value * unit.getConversionFactor();
 		}
 
+		// Static method to convert to target type
+		public static double convert(double value, LengthUnit source, LengthUnit target) {
+
+			// Validation
+			if (source == null || target == null) {
+				throw new IllegalArgumentException("Unit cannot be null");
+			}
+
+			if (!Double.isFinite(value)) {
+				throw new IllegalArgumentException("Value must be finite");
+			}
+
+			// Normalize to base unit
+			double baseValue = value * source.getConversionFactor();
+
+			// Convert to target unit
+			return baseValue / target.getConversionFactor();
+		}
+
+		// Instance conversion method 
+		public QuantityLength convertTo(LengthUnit target) {
+
+	        double convertedValue = convert(this.value, this.unit, target);
+
+	        return new QuantityLength(convertedValue, target);
+	    }
+		// Overriding equals method to compare two QuantityLength objects
 		@Override
 		public boolean equals(Object obj) {
 
+			// Checking same reference - Reflexive property
 			if (this == obj)
 				return true;
 
+			// Checking null and class type
 			if (obj == null || getClass() != obj.getClass())
 				return false;
 
+			// Type casting
 			QuantityLength other = (QuantityLength) obj;
 
 			// Comparing values after converting to base unit
-	        double difference = Math.abs(
-	                this.convertToBaseUnit() - other.convertToBaseUnit()
-	        );
+			double difference = Math.abs(this.convertToBaseUnit() - other.convertToBaseUnit());
 
-	        return difference < 0.0001;
+			return difference < 0.0001;
 		}
 
+		// Overriding hashCode method - consistent with equals
 		@Override
 		public int hashCode() {
-			return Objects.hash(convertToBaseUnit());
+
+			return Objects.hash(Math.round(convertToBaseUnit() * 10000));
 		}
 
+		// Overriding toString method for readable output
 		@Override
 		public String toString() {
 			return "Quantity(" + value + ", " + unit + ")";
 		}
+
 }

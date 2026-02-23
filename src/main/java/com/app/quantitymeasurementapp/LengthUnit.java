eea37b6811a02a43cdc9e401f79b6310b5b75331
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-02-23 09:48:12 +0530

    [Riya]: Implemented YARDS and CENTIMETERS constants in LengthUnit enum class with proper conversion factors

--- src/main/java/com/app/quantitymeasurementapp/LengthUnit.java
+++ src/main/java/com/app/quantitymeasurementapp/LengthUnit.java
@@ -1,19 +1,16 @@
 package com.app.quantitymeasurementapp;
 
 public enum LengthUnit {
-	// Base unit :Feet, Inches convert to feet 
-		FEET(1.0), INCH(1.0 / 12.0);
-		
-		// Conversion factor
-		private final double conversionFactor;
+	INCH(1.0), FEET(12.0), YARDS(36.0), CENTIMETERS(0.393701);
 
-		//Constructor
-	    LengthUnit(double conversionFactor) {
-	        this.conversionFactor = conversionFactor;
-	    }
+	private final double conversionFactor;
 
-	    // Converting to base unit
-	    public double getConversionFactor() {
-	        return conversionFactor;
-	    }
+	// Constructor
+	LengthUnit(double conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
 }

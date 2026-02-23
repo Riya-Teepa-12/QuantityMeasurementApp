d6607fc758c25d335a5859e705e1f61aa4b7f0d3
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-02-23 12:24:55 +0530

    [Riya]:  Implemented YARDS and CENTIMETERS support in LengthUnit enum with conversion factors

--- src/main/java/com/app/quantitymeasurementapp/LengthUnit.java
+++ src/main/java/com/app/quantitymeasurementapp/LengthUnit.java
@@ -3,6 +3,7 @@ package com.app.quantitymeasurementapp;
 public enum LengthUnit {
 	INCH(1.0), FEET(12.0), YARDS(36.0), CENTIMETERS(0.393701);
 
+	// Conversion factor
 	private final double conversionFactor;
 
 	// Constructor
@@ -10,6 +11,7 @@ public enum LengthUnit {
 		this.conversionFactor = conversionFactor;
 	}
 
+	// Converting to base unit
 	public double getConversionFactor() {
 		return conversionFactor;
 	}

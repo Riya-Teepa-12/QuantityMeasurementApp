6526f16274fb190a20fb971e07b5b4e94d312e95
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-02-24 07:23:16 +0530

    [Riya]: UC12: Implement subtraction and division for Quantity measurements

--- src/main/java/com/app/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -3,8 +3,6 @@ package com.app.quantitymeasurementapp;
 
 
 public class QuantityMeasurementApp {
-
-
 	// Common display utility method
 		private static void displayResult(String operation, Object result) {
 			System.out.println(operation + " : " + result);
@@ -81,11 +79,42 @@ public class QuantityMeasurementApp {
 			displayResult("Round Trip Test (" + quantity + ")", roundTrip);
 		}
 
+		// Subtraction demonstration
+		public static <U extends IMeasurable> Quantity<U> demonstrateSubtraction(
+				Quantity<U> q1, Quantity<U> q2) {
+
+			if (q1 == null || q2 == null) {
+				throw new IllegalArgumentException("Quantities cannot be null");
+			}
+
+			Quantity<U> result = q1.subtract(q2);
+
+			displayResult("Subtraction (" + q1 + " - " + q2 + ")", result);
+
+			return result;
+		}
+		
+		// Division demonstration
+		public static <U extends IMeasurable> double demonstrateDivision(
+				Quantity<U> q1, Quantity<U> q2) {
+
+			if (q1 == null || q2 == null) {
+				throw new IllegalArgumentException("Quantities cannot be null");
+			}
+
+			double result = q1.divide(q2);
+
+			displayResult("Division (" + q1 + " / " + q2 + ")", result);
+
+			return result;
+		}
+		
+		
 		// MAIN METHOD
 		public static void main(String[] args) {
 
 			// Length method
-			System.out.println("----------------------------------------------");
+			System.out.println("***********************************************");
 			Quantity<LengthUnit> length1 = new Quantity<>(1.0, LengthUnit.FEET);
 
 			Quantity<LengthUnit> length2 = new Quantity<>(12.0, LengthUnit.INCH);
@@ -95,8 +124,12 @@ public class QuantityMeasurementApp {
 			demonstrateAddition(length1, length2);
 			demonstrateAddition(length1, length2, LengthUnit.FEET);
 
+			// new updated methods 
+			demonstrateSubtraction(length1, length2);
+			demonstrateDivision(length1, length2);
+			
 			// Weight method
-			System.out.println("---------------------------------------------");
+			System.out.println("***********************************************");
 			Quantity<WeightUnit> weight1 = new Quantity<>(1.0, WeightUnit.KILOGRAM);
 
 			Quantity<WeightUnit> weight2 = new Quantity<>(1000.0, WeightUnit.GRAM);
@@ -110,9 +143,12 @@ public class QuantityMeasurementApp {
 			demonstrateAddition(weight1, weight3, WeightUnit.GRAM);
 			demonstrateRoundTrip(weight1, WeightUnit.POUND);
 
+			// new updates 
+			demonstrateSubtraction(weight1, weight3);
+			demonstrateDivision(weight1, weight3);
 			
 			// Volume method 
-			System.out.println("----------------------------------------------");
+			System.out.println("***********************************************");
 			Quantity<VolumeUnit> volume1 = new Quantity<>(1.0, VolumeUnit.LITRE);
 
 			Quantity<VolumeUnit> volume2 = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
@@ -128,7 +164,12 @@ public class QuantityMeasurementApp {
 			demonstrateAddition(volume1, volume3);
 			demonstrateAddition(volume1, volume3, VolumeUnit.MILLILITRE);
 			demonstrateRoundTrip(volume1, VolumeUnit.GALLON);
-			System.out.println("----------------------------------------------");
+			demonstrateSubtraction(volume1, volume2);
+			demonstrateDivision(volume1, volume2);
+
+			demonstrateSubtraction(volume1, volume3);
+			demonstrateDivision(volume1, volume3);
+			System.out.println("***********************************************");
 
 		}
 }

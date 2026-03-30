f633bc591ce149ef94defe78b50fe41a6ed0378f
Riya-Teepa-12 <teepavlogsriya@gmail.com>
2026-03-31 02:31:53 +0530

    [Riya]: UC18: add SignInDto, SignUpDto, AuthResponseDTO and update QuantityMeasurementDTO

---
+++ src/main/java/com/app/quantitymeasurement/dto/SignInDto.java
@@ -0,0 +1,10 @@
+package com.app.quantitymeasurement.dto;
+
+import lombok.Data;
+
+@Data
+public class SignInDto {
+public String email;
+public String password;
+}
+
---
+++ src/main/java/com/app/quantitymeasurement/dto/SignUpDto.java
@@ -0,0 +1,10 @@
+package com.app.quantitymeasurement.dto;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+  public String name;
+  public String email;
+  public String password;
+}
